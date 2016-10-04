ebf77961b6890358131dc41b625e41acb2fdf303
nishkumar <nk595@cornell.edu>
2016-10-03 23:50:46 -0400

    fix warnings

--- pytree.py
+++ pytree.py
@@ -4,38 +4,38 @@ import sys
 import os
 import re
 
-#Function to generate sort key
-#Simply convert files names to lowercase
-#This way its easier to sort by ignoring case and special chars.
+
+# Function to generate sort key
+# Simply convert files names to lowercase
+# This way its easier to sort by ignoring case and special chars.
 def get_key(s):
      return re.sub('[^A-Za-z0-9]+', '', s).lower()
 
 
-
-#Recursive function to print file tree 
+# Recursive function to print file tree 
 def draw_tree(path, count, indent = ''):
     rawList = os.listdir(path)
     fileList = []
 
-    #Discard hidden files		    
+    # Discard hidden files
     for file in rawList:
         if(not file.startswith(".")):
-           fileList.append(file)
+            fileList.append(file)
 
-    #sort files in ascending order
+    # sort files in ascending order
     fileList = sorted(fileList, key=get_key)
-    size=0
+    size = 0
 
     for fileName in fileList:
-        childPath  = os.path.join(path, fileName)
-        #Print file name irrespective of file or dir
+        childPath = os.path.join(path, fileName)
+        # Print file name irrespective of file or dir
         size = size + 1
         if size == len(fileList):
             print(indent + "`-- " + fileName)
         else:
             print(indent + "|-- " + fileName)
         
-        #If its a directory do a recursive search
+        # If its a directory do a recursive search
         if os.path.isdir(childPath):
             count["dirs"] = count["dirs"] + 1
             if size == len(fileList):
@@ -43,15 +43,15 @@ def draw_tree(path, count, indent = ''):
             else:
                 draw_tree(childPath, count, indent + '|   ')
         else:
-            #Is a file
+            # Is a file
             count["files"] = count["files"] + 1
 
 
 
 
-############################  MAIN  ############################
-#count the number of files and dirs
-count  = {"files":0,"dirs":0}
+# ******************************  MAIN  ****************************** 
+# Count the number of files and dirs
+count = {"files": 0,"dirs": 0}
 
 if len(sys.argv) == 1:
     print('.')
